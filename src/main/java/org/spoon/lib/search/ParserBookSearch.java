d6b44efc4efbb4f4236e892842fcdc0c2c7c9f53
mookseong <kmkook12312@gmail.com>
2023-02-16 15:33:44 +0900

    comment : Add Interface comment

--- src/main/java/org/spoon/lib/search/ParserBookSearch.java
+++ src/main/java/org/spoon/lib/search/ParserBookSearch.java
@@ -20,27 +20,81 @@ public interface ParserBookSearch {
      */
     List<BookSearch> getBookSearchListBySearch();
 
+    /**
+     * 검색된 책 이미지 정보만 가져옵니다.
+     * @return 책 이미지 정보를 담고 있는 List 반환합니다.
+     */
     List<String> getBookImg();
 
+    /**
+     * 특정위치 책위치 정보를 불러옵니다.
+     * @param index 검색 위치 정보
+     * @return 책 이미지 정보를 담고 있는 정보를 반환합니다.
+     */
     String getBookImg(int index);
 
+    /**
+     * 검색된 일부 정보를 담고 있는 List 반환합니다.
+     * @return 책 정보를 담고 있는 List 반환합니다.
+     */
     List<String> getBookInformation();
 
+    /**
+     * 특정위치에 있는 정보를 불러옵니다.
+     * @param index 검색 위치 정보
+     * @return 책 특정 위치 정보를 반환합니다.
+     */
     String getBookInformation(int index);
 
+    /**
+     * 책 제목만 불러옵니다.
+     * @return 책 정보를 담고 있는 List 반환합니다.
+     */
     List<String> getBookTitle();
 
+    /**
+     * 특정위치에 있는 정보를 불러옵니다.
+     * @param index 검색 위치 정보
+     * @return 책 특정 위치 정보를 반환합니다.
+     */
     String getBookTitle(int index);
 
+    /**
+     * 책 isbn 정보를 불러옵니다.
+     * @return 책 isbn 정보를 담고 있는 List 반환합니다.
+     */
     List<String> getBookIsbn();
 
+    /**
+     * 특정위치에 있는 정보를 불러옵니다.
+     * @param index 검색 위치 정보
+     * @return 책 특정 위치 정보를 반환합니다.
+     */
     String getBookIsbn(int index);
 
+    /**
+     * 책 기타 정보를 불러옵니다.
+     * @return 책 기타 정보를 담고 있는 List 반환합니다.
+     */
     List<String> getBookEtc();
 
+    /**
+     * 특정위치에 있는 정보를 불러옵니다.
+     * @param index 검색 위치 정보
+     * @return 책 특정 위치 정보를 반환합니다.
+     */
     String getBookEtc(int index);
 
+    /**
+     * 책 대여정보를 정보를 불러옵니다.
+     * @return 책 기타 정보를 담고 있는 List 반환합니다.
+     */
     List<BookKeepInfo> getKeepBookList();
 
+    /**
+     * 특정위치에 있는 책 대여정보 정보를 불러옵니다.
+     * @param index 검색 위치 정보
+     * @return 책 특정 위치 정보를 반환합니다.
+     */
     List<BookKeepInfo> getKeepBookList(int index);
 }
